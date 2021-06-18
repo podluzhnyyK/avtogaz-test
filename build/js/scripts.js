173b0af5523edefcde6254acfa661fce5dcb38cc
podluzhnyyK <podluzhnyy94@mail.ru>
2021-06-18 18:15:26 +0500

    добавил маску на input tel

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,6 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable max-len */
+/* eslint-disable no-invalid-this */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 // tabs
@@ -204,7 +207,47 @@ const swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 
-    // autoplay: {
-    //     delay: 3000,
-    // },
+    autoplay: {
+        delay: 3000,
+    },
 });
+
+// mask tel
+function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+    const elems = document.querySelectorAll(selector);
+
+    function mask(event) {
+        const keyCode = event.keyCode;
+        const template = masked;
+        const def = template.replace(/\D/g, '');
+        const val = this.value.replace(/\D/g, '');
+        console.log(template);
+        let i = 0;
+        let newValue = template.replace(/[_\d]/g, function(a) {
+            return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+        });
+        i = newValue.indexOf('_');
+        if (i !== -1) {
+            newValue = newValue.slice(0, i);
+        }
+        let reg = template.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return '\\d{1,' + a.length + '}';
+            }).replace(/[+()]/g, '\\$&');
+        reg = new RegExp('^' + reg + '$');
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+            this.value = newValue;
+        }
+        if (event.type === 'blur' && this.value.length < 5) {
+            this.value = '';
+        }
+    }
+
+    for (const elem of elems) {
+        elem.addEventListener('input', mask);
+        elem.addEventListener('focus', mask);
+        elem.addEventListener('blur', mask);
+    }
+}
+
+maskPhone('#tel');
